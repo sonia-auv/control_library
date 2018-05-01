b04688ee7e213c55ea5310c23e58db2ecc961ee7
olavoie <olavoie9507@gmail.com>
2018-04-30 20:39:45 -0400

    adding header

--- test/TransferFunctionTest.cc
+++ test/TransferFunctionTest.cc
@@ -1,3 +1,28 @@
+/**
+ * \file	TransferFunctionTest.cc
+ * \author	Olivier Lavoie <olavoie9507@gmail.com>
+ * \date	10/21/17
+ *
+ * \copyright Copyright (c) 2018 S.O.N.I.A. AUV All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. AUV software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. AUV software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. AUV software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <gtest/gtest.h>
 #include "toolbox/TransferFunction.h"
 #include <memory>
@@ -45,7 +70,7 @@ TEST_F(TransferFunction_Unit_Test, TransferFunction2Order)
 
     for (int i = 0; i < nbDegreeOfFreedom_; i++)
     {
-        denominatorFactor_.row(i) << -1.970052211604768,  0.970446261452074;
+        denominatorFactor_.row(i) << -1.970052211604768, 0.970446261452074;
         numeratorFactor_.row(i)   << 0.004925623091321, 0.009851246182642, 0.004925623091321;
     }
 
