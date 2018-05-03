fd0f250b7b2f9d8c2afdf13a6d1048f8688b3900
olavoie <olavoie9507@gmail.com>
2018-05-03 10:26:58 -0400

    Revert "finish trajectory generator for orientation"
    This reverts commit 6d8df9d742dd75b65d84f0a6083f661b82670c9d.

--- src/toolbox/Transformation.h
+++ src/toolbox/Transformation.h
@@ -1,38 +1,8 @@
-/**
- * \file	Transformation.h
- * \author	Olivier Lavoie <olavoie9507@gmail.com>
- * \date	10/21/17
- *
- * \copyright Copyright (c) 2018 S.O.N.I.A. AUV All rights reserved.
- *
- * \section LICENSE
- *
- * This file is part of S.O.N.I.A. software.
- *
- * S.O.N.I.A. AUV software is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * S.O.N.I.A. AUV software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with S.O.N.I.A. AUV software. If not, see <http://www.gnu.org/licenses/>.
- */
+//
+// Created by olavoie on 5/2/18.
+//
 
 #ifndef CONTROL_LIBRARY_TRANSFORMATION_H
 #define CONTROL_LIBRARY_TRANSFORMATION_H
-
-#include <eigen3/Eigen/Eigen>
-
-namespace control
-{
-    Eigen::Quaterniond EulerToQuaternion(Eigen::Vector3d &eulerAngle);
-    Eigen::Matrix3d    EulerToRotationMatrix(Eigen::Vector3d &eulerAngle);
-
-}
 
 #endif //CONTROL_LIBRARY_TRANSFORMATION_H
