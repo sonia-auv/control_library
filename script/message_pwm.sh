929f04eb59199bda5c0d2032933bab610d68ab1c
alamarre3 <alexandrelamarre52@gmail.com>
2022-09-25 17:52:37 -0400

    remove unused files

--- script/message_pwm.sh
+++
@@ -1,2 +0,0 @@
-rostopic pub --once /provider_thruster/thruster_pwm sonia_common/ThrusterPwm '{pwm:[1560, 1500, 1500, 1500, 1500, 1500, 1500, 1500]}'
-rostopic pub --once /provider_thruster/thruster_pwm sonia_common/ThrusterPwm '{pwm:[1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500]}'
